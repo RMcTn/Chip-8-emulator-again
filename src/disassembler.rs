ae5a48ab7092a990336e49ffb651498c9ce53bc4
RMcTn <rmctiernan@protonmail.com>
2023-05-06 22:11:39 +0100

    Add disassembly for LD I

--- src/disassembler.rs
+++ src/disassembler.rs
@@ -1,7 +1,7 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, num::ParseIntError};
 
 fn make_instruction_to_opcode_mapping() -> HashMap<&'static str, u8> {
-    HashMap::from([("JP", 0x1)])
+    HashMap::from([("JP", 0x1), ("LD I", 0xA)])
 }
 
 pub fn disassemble(lines: Vec<String>) -> Vec<u8> {
@@ -12,9 +12,46 @@ pub fn disassemble(lines: Vec<String>) -> Vec<u8> {
     // Is that just a bunch of typing for nothing?
     let mut machine_code: Vec<u8> = Vec::new();
     for line in lines {
+        dbg!(&line);
         let splits: Vec<&str> = line.split_whitespace().collect();
         match splits.len() {
-            3 => {} // Do that 3 split parsing stuff
+            3 => {
+                let first_token = splits[0];
+                let second_token = splits[1];
+                if second_token.chars().last().unwrap() != ',' {
+                    // Any opcode with 3 parts always has a comma at the end of the 2nd token.
+                    // Could tokenize this better if we really wanted, but this gets us moving
+                    // quicker
+                    // TODO(reece): Error here
+                    panic!("Expected ',' after first argument for opcode");
+                }
+                let second_token = &second_token[0..second_token.len() - 1];
+                let third_token = splits[2];
+                match first_token {
+                    "LD" => {
+                        match second_token.chars().next().unwrap() {
+                            'I' => {
+                                let opcode = *instruction_to_opcode_map.get("LD I").unwrap();
+                                let without_prefix = third_token.trim_start_matches("0x");
+                                // TODO(reece): Validate as hexadecimal (Ignore size of number for now)
+                                let addr = u16::from_str_radix(without_prefix, 16).unwrap();
+                                let last_byte = (addr & 0xFF) as u8;
+                                let addr_nibble = (addr >> 8 & 0xF) as u8;
+                                let first_byte = (opcode << 4) | addr_nibble;
+
+                                machine_code.push(first_byte);
+                                machine_code.push(last_byte);
+                            }
+                            _ => {
+                                todo!();
+                            }
+                        }
+                    }
+                    _ => {
+                        todo!();
+                    }
+                }
+            }
             2 => {
                 let first_token = splits[0];
                 let second_token = splits[1];
