8841e54e51c9f6c5336a8f8f1cadada26a4d1911
RMcTn <rmctiernan@protonmail.com>
2023-07-02 01:28:24 +0100

    Remove unused code

--- src/assembler.rs
+++ src/assembler.rs
@@ -35,25 +35,6 @@ pub enum TokenType {
     Register,
 }
 
-enum OpcodeType {
-    // The plan here was write something that could hold the different types of arguments we get.
-    // For example:
-    // LD I, 0x200
-    // LD 0x1, 0x3
-    // JP 0x200
-    // Then our TokenTypes could be
-    // LD(OpcodeType) -> match on this when doing code generation
-    // match opcode_type {
-    //  I register -> that stuff
-    //  2 arguments -> 2 arg stuff
-    // }
-    // But this might be confusing what a "token" really means. These almost feel more like
-    // statements.
-    // Is it necessary to have a tokenizing pass over the assembly code, and then a pass over the
-    // tokens to generate "statements", then another pass of statements just to generate machine
-    // code for such a simple instruction set?
-}
-
 #[derive(Debug, Clone)]
 pub struct Token {
     pub token_type: TokenType,
@@ -297,6 +278,8 @@ impl Parser {
     /// Any instruction that uses a register specified by hexadecimal will be assumed to be valid
     /// for now
     fn generate_machine_code(&mut self) -> Vec<u8> {
+        // Didn't feel necessary to generate "statements" from the tokens just to generate the
+        // machine code at the time
         let mut machine_code = Vec::with_capacity(100);
         while self.current < self.tokens.len() {
             println!("Machine code so far {:X?}", &machine_code);
@@ -920,86 +903,4 @@ impl Parser {
             return false;
         }
     }
-}
-
-pub fn assemble_old(lines: Vec<String>) -> Vec<u8> {
-    // Assume no labels for now
-    let instruction_to_opcode_map = make_instruction_to_opcode_mapping();
-
-    // Could do this as enums of what the instructions are, then output something at the end?
-    // Is that just a bunch of typing for nothing?
-    let mut machine_code: Vec<u8> = Vec::new();
-    for line in lines {
-        dbg!(&line);
-        let splits: Vec<&str> = line.split_whitespace().collect();
-        match splits.len() {
-            3 => {
-                let first_token = splits[0];
-                let second_token = splits[1];
-                if second_token.chars().last().unwrap() != ',' {
-                    // Any opcode with 3 parts always has a comma at the end of the 2nd token.
-                    // Could tokenize this better if we really wanted, but this gets us moving
-                    // quicker
-                    // TODO(reece): Error here
-                    panic!("Expected ',' after first argument for opcode");
-                }
-                let second_token = &second_token[0..second_token.len() - 1];
-                let third_token = splits[2];
-                match first_token {
-                    "LD" => {
-                        match second_token.chars().next().unwrap() {
-                            'I' => {
-                                let opcode = *instruction_to_opcode_map.get("LD I").unwrap();
-                                let without_prefix = third_token.trim_start_matches("0x");
-                                let addr = u16::from_str_radix(without_prefix, 16).unwrap();
-                                let last_byte = (addr & 0xFF) as u8;
-                                let addr_nibble = (addr >> 8 & 0xF) as u8;
-                                let first_byte = (opcode << 4) | addr_nibble;
-
-                                machine_code.push(first_byte);
-                                machine_code.push(last_byte);
-                            }
-                            'V' => {
-                                // This is starting to feel annoying having to validate constantly.
-                                // Maybe should just parse as 1 pass, then convert those tokens
-                                // into machine code?
-                            }
-                            _ => {
-                                todo!();
-                            }
-                        }
-                    }
-                    _ => {
-                        todo!();
-                    }
-                }
-            }
-            2 => {
-                let first_token = splits[0];
-                let second_token = splits[1];
-                match first_token {
-                    "JP" => {
-                        let opcode = *instruction_to_opcode_map.get(first_token).unwrap();
-                        let without_prefix = second_token.trim_start_matches("0x");
-                        // TODO(reece): Validate as hexadecimal (Ignore size of number for now)
-                        let addr = u16::from_str_radix(without_prefix, 16).unwrap();
-                        let last_byte = (addr & 0xFF) as u8;
-                        let addr_nibble = (addr >> 8 & 0xF) as u8;
-                        let first_byte = (opcode << 4) | addr_nibble;
-
-                        machine_code.push(first_byte);
-                        machine_code.push(last_byte);
-                    }
-                    _ => {
-                        todo!()
-                    }
-                }
-            }
-            _ => {
-                todo!()
-            } // TODO(reece): Error when not 2 or 3
-        }
-    }
-
-    return machine_code;
 }
